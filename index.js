626de6b4f4d125aceb6339a5d3fda7755d60ebc0
SureshKuchana <kuchana123.suresh@gmail.com>
2021-03-18 08:25:11 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log("Suresh Koochana -- Software Engineer");
+console.log(`
+Suresh Koochana
+
+Is a Passionate Software Engineer with many interests,
+especially in Web Development. 
+Curiosity and the ability to learn new technologies and tools.
+I 'm continuously seeking new challenges and a desire to expand knowledge e and experience.
+
+Twitter : https://twitter.com/Suresh_Koochana
+Github : https://github.com/SureshKuchana
+Blog : https://suresh123.herokuapp.com
+`);
